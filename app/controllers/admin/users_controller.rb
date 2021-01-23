67e5919d1dd6f607e779c5fbf5f1d30e1ddea772
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-23 11:04:12 +0900

    Add new and create action in users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def edit
@@ -9,5 +10,17 @@ class Admin::UsersController < ApplicationController
   end
 
   def index
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}を登録しました」"
+    else
+      render :new
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
 end
